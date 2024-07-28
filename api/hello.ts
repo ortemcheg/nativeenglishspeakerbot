24796ba3689fe00b96b73fd4b369987ff82ea1ce
Tyoma Deev <tyoma.deev@gmail.com>
2024-07-28 15:44:24 +0200

    reacting to post requests

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,4 @@
-export function GET(request: Request) {
+export function POST(request: Request) {
   return new Response(JSON.stringify({ hello: "world" }), {
     headers: new Headers({
       "Content-Type": "application/json",
