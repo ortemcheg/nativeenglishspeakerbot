c7b9feaefe29b8c5b21d0875597bd97e1771e224
Tyoma Deev <tyoma.deev@gmail.com>
2024-07-28 16:28:10 +0200

    Testing replies

--- api/hello.ts
+++ api/hello.ts
@@ -6,7 +6,7 @@ if (!token) throw new Error("BOT_TOKEN is unset");
 const bot = new Bot(token);
 
 bot.on("message", async (ctx) => {
-  await ctx.reply("Hi there!");
+  await ctx.reply(`You said ${ctx.message.text}`);
 });
 
 export default webhookCallback(bot, "https");
