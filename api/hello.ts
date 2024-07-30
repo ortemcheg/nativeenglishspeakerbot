cc86b2a13566c8e51e15cbc1ab02d37f441e9c4c
Tyoma Deev <tyoma.deev@gmail.com>
2024-07-30 10:34:01 +0200

    Adds session storage

--- api/hello.ts
+++ api/hello.ts
@@ -1,12 +1,30 @@
-import { Bot, webhookCallback } from "grammy";
+import { Bot, Context, session, SessionFlavor, webhookCallback } from "grammy";
+import { freeStorage } from "@grammyjs/storage-free";
+
+interface SessionData {
+  messageCount: number;
+}
+
+type MyContext = Context & SessionFlavor<SessionData>;
 
 const token = process.env.BOT_TOKEN;
 if (!token) throw new Error("BOT_TOKEN is unset");
 
-const bot = new Bot(token);
+const bot = new Bot<MyContext>(token);
+
+bot.use(
+  session({
+    initial: () => ({ messageCount: 0 }),
+    storage: freeStorage<SessionData>(bot.token),
+  })
+);
 
 bot.on("message", async (ctx) => {
-  await ctx.reply(`${ctx.from.first_name} said "${ctx.message.text}"`);
+  ctx.session.messageCount++;
+  await ctx.reply(
+    `${ctx.session.messageCount}. ${ctx.from.first_name} said "${ctx.message.text}"`
+  );
+  ctx.session.messageCount++;
 });
 
 export default webhookCallback(bot, "https");
